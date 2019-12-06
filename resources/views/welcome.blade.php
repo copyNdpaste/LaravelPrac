3352b787262cd2b0a69559df409fd4914382a509
simon <julsnutini@naver.com>
2019-12-06 18:11:23 +0900

    routes and send data to view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,10 @@
 @extends('layout')  <!--layout.blade.php 템플릿 사용-->
 
 @section('content')  <!--작성하고자 하는 내용을 쓰고 layout.blade.php의 content에 갖다 붙인다.-->
-    <h1>My Web</h1>
+    <h1>{{ $foo }}</h1>
+    <ul>
+        @foreach($tasks as $task)
+            <li>{{ $task }}</li>
+        @endforeach
+    </ul>
 @endsection
