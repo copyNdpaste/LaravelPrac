01d1508db86e789a52d01677b52fe026668fe5c7
simon <julsnutini@naver.com>
2019-12-06 23:37:20 +0900

    rm project

--- LaravelPrac/resources/views/contact.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout')
-
-@section('title', 'contact us')
-
-@section('content')
-<h1>This is contact page</h1>
-@endsection
--- LaravelPrac/resources/views/welcome.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layout')  <!--layout.blade.php 템플릿 사용-->
-
-@section('content')  <!--작성하고자 하는 내용을 쓰고 layout.blade.php의 content에 갖다 붙인다.-->
-    <h1>{{ $foo }}</h1>
-@endsection
