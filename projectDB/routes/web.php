31d1970b08cceb7748e9a47a2ad4ae9df1561ea5
simon <julsnutini@naver.com>
2019-12-09 12:10:39 +0900

    simplify routes

--- projectDB/routes/web.php
+++ projectDB/routes/web.php
@@ -1,20 +1,24 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/projects', 'ProjectController@index');  # /project에 요청이 들어오면 ProjectController의 index 메서드가 실행됨.
-Route::post('/projects', 'ProjectController@store');
-Route::get('/projects/create', 'ProjectController@create');
+/*
+    GET /projects (index)
+    GET /projects/create (create)
+    GET /projects/1 (show)
+    POST /projects (store)
+    GET /projects/1/edit (edit)
+    PATCH /project/1 (update)
+    DELETE /projects/1 (destroy)
+*/
+
+Route::resource('projects', 'ProjectsController');  # route shortcut
+//Route::get('/projects', 'ProjectsController@index');  # project에 요청이 들어오면 ProjectsController의 index 메서드가 실행됨.
+//Route::get('/projects/create', 'ProjectsController@create');
+//Route::get('/projects/{project}', 'ProjectsController@show');
+//Route::post('/projects', 'ProjectsController@store');
+//Route::get('/projects/{project}/edit', 'ProjectsController@edit');
+//Route::patch('/projects/{project}', 'ProjectsController@update');
+//Route::delete('/projects/{project}', 'ProjectsController@destroy');
