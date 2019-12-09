7c089296934b511d7ec2ca1e37327e658f62cdf3
simon <julsnutini@naver.com>
2019-12-09 12:09:30 +0900

    layout

--- projectDB/resources/views/layout.blade.php
+++ projectDB/resources/views/layout.blade.php
@@ -1,1 +1,12 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <title></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
